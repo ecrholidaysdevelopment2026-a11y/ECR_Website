54964d82b9fe0941d5177576e246623bc7df45e8
Prakash-WebDad <prakash.s@webdads2u.com>
2025-12-16 16:23:36 +0530

    git error solve

--- app/components/Container/HomeSection/Destination/Destination.jsx
+++ app/components/Container/HomeSection/Destination/Destination.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react'
 import DestinationsChennai from './DestinationsChennai'
 import DestinationsPudhucherry from './DestinationsPudhucherry'
@@ -17,24 +16,4 @@ const Destination = () => {
     )
 }
 
-=======
-import React from 'react'
-import DestinationsChennai from './DestinationsChennai'
-import DestinationsPudhucherry from './DestinationsPudhucherry'
-import TrendingSeason from './TrendingSeason'
-import LuxuryBegins from './LuxuryBegins'
-
-const Destination = () => {
-    return (
-        <div className="px-3 md:px-30">
-            <DestinationsChennai />
-            <DestinationsPudhucherry />
-            <TrendingSeason />
-            <LuxuryBegins />
-        </div>
-
-    )
-}
-
->>>>>>> 3117bdd47e565d954c51a6a685d0a22fad1fb592
 export default Destination
